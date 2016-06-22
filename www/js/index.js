c4a1591ff4a48cc36449ba8fa2023e7fdaa08877
Lepikov Stanislav <s.lepikov@semrush.com>
2016-06-22 12:09:58 +0300

    add menu;add cities list

--- www/js/index.js
+++ www/js/index.js
@@ -19,47 +19,50 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        this.initElems();
         this.bindEvents();
-        app.loadDoc('http://export.yandex.ru/weather-ng/forecasts/2.xml', function(result) {
-            console.log(result);
-        })
     },
 
-    loadDoc: function (path, cb) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                cb(xhttp);
-            }
-        };
-        xhttp.open("GET", path, true);
-        xhttp.send();
+    initElems: function() {
+        this.menu = $('#menu');
+        this.openMenuEl = $('#open-menu');
+        this.buttonMenu = $('#button-menu');
+        this.savedCities = $('#savedCities li');
+        this.cityName = $('#cityName');
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        //app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        this.buttonMenu.on('tap', $.proxy(this.openMenu, this));
+        this.savedCities.on('tap', $.proxy(function(e) {
+            var $target = $(e.target),
+                opts = {
+                    cityName: $target.text()
+                };
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+            this.loadCityWeather(opts);
+        }, this));
 
-        console.log('Received Event: ' + id);
+        this.menu.on('swipeleft', $.proxy(this.closeMenu, this));
+        this.openMenuEl.on('swiperight', $.proxy(this.openMenu, this));
+    },
+
+    loadCityWeather: function(params) {
+        this.changeCityName(params.cityName);
+        this.closeMenu()
+    },
+
+    changeCityName: function (cityName) {
+        this.cityName.text(cityName);
+    },
+
+    openMenu: function() {
+        this.menu.addClass('open');
+    },
+    closeMenu: function() {
+        this.menu.removeClass('open');
     }
 };
 
-app.initialize();
+$(function() {
+    app.initialize();
+});
